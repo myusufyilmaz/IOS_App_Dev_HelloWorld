3ed9adf68a1711256a53a4c31c0d140706e8e287
template <myilmaz2@cot-sl320-12.local>
2018-08-22 14:36:37 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Yilmaz, Muhammed Yusuf on 8/22/18.
+//  Copyright © 2018 Yilmaz, Muhammed Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
